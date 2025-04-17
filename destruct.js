ff2fb6b8b8c4082ed7666bc3246b826a37413c6c
yusufbahtiarr <96324893+yusufbahtiarr@users.noreply.github.com>
2025-04-17 11:30:15 +0700

    Update destruct.js

--- destruct.js
+++ destruct.js
@@ -7,8 +7,8 @@ const data = [
 
 ]
 
-const {_, value } = data[0][1][3].string[1]
+// const {_, value } = data[0][1][3].string[1]
 // const {string:[_a,{value}]} = data[0][1][3]
-// const [[_a,[_b,_c,_d,{string:[_e,{value}]}]]] = data
+const [[_a,[_b,_c,_d,{string:[_e,{value}]}]]] = data
 
 console.log(value);
