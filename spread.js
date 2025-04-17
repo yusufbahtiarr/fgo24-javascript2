ba693d49510c2ece8b192bc98c466750c2665b5f
yusufbahtiarr <96324893+yusufbahtiarr@users.noreply.github.com>
2025-04-17 10:00:35 +0700

    Update spread.js

--- spread.js
+++ spread.js
@@ -16,10 +16,42 @@ data[0][1][3].string[1] = {
 
 const data2 = {
     ...data,
-    value : "Selamat Pagi"
+    value : "Selamat Pagi",
 }
 
 console.log(data[0][1][3].string[1].value); // Selamat
 console.log(data2[0][1][3].string[1].value); // Selamat
 
 
+const bio = {
+    fullName: "John O'Connor",
+    age: 30
+}
+
+const extraInfo = {
+    skills: ["Web Programming", "Painting"],
+    educations: [
+        {
+            name: "SMP 1 Denpasar",
+            year: 2012
+        }
+    ]
+}
+
+const newData = {
+    skills: ['Hiking'],
+    educations: [{
+        name: "SMP 1 Jakarta",
+    year: 2000 
+    }]
+}
+ 
+extraInfo.skills = [...extraInfo.skills, ...newData.skills]
+extraInfo.educations = [...extraInfo.educations, ...newData.educations]
+
+const profile = {
+    ...bio,
+    ...extraInfo
+}
+
+console.log(profile);
