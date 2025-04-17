3356b2f217981d76ac6db91c8b43926d7509cb0e
yusufbahtiarr <96324893+yusufbahtiarr@users.noreply.github.com>
2025-04-17 11:35:20 +0700

    Add files via upload

---
+++ minitask1.js
@@ -0,0 +1,9 @@
+function oddEvenNumber(numbers){
+    if (numbers % 2 == 0){
+        console.log("Bilangan Genap");
+    }else {
+        console.log("Bilangan Ganjil");
+    }
+}
+
+oddEvenNumber(3)
