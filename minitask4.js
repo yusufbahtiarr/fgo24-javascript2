16ada4034a3514ed778b14c8b61849ee6c91179b
yusufbahtiarr <96324893+yusufbahtiarr@users.noreply.github.com>
2025-04-17 14:48:41 +0700

    Update minitask4.js

--- minitask4.js
+++ minitask4.js
@@ -1,15 +1,14 @@
 // Menentukan bilangan ganjil dan genap dengan callback
 
 const oddEvenNumber = (num, cv) =>{
-    return cv(num)
-}
-
-const oddEven = (x) => {
     if (typeof x != "number"){
         console.log("Input harus bertipe number");
         return
     }
+    return cv(num)
+}
 
+const oddEven = (x) => {
     if (x % 2 == 0 ){
         console.log("Bilangan Genap");
     } else {
