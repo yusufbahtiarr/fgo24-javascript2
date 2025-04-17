c97fb5aad34235b18f7cdfee9f019337875b62f1
yusufbahtiarr <96324893+yusufbahtiarr@users.noreply.github.com>
2025-04-17 11:29:13 +0700

    Add files via upload

---
+++ destruct.js
@@ -0,0 +1,14 @@
+const data = [
+    [ ,[ , , , {
+        string: [ ,{
+            value : "hello"
+        }]}]
+    ]
+
+]
+
+const {_, value } = data[0][1][3].string[1]
+// const {string:[_a,{value}]} = data[0][1][3]
+// const [[_a,[_b,_c,_d,{string:[_e,{value}]}]]] = data
+
+console.log(value);
